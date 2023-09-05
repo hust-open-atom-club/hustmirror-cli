28779946ae5f9754a3c95e0d9a756c2e890f87a1
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2023-09-05 15:49:20 +0800

    fix: fix cli bugs.

--- src/cli.sh
+++ src/cli.sh
@@ -44,12 +44,6 @@ cli_main() {
 			print_error "Unknown argument $1, exit."
 			;;
 	esac
-
-
-	# install
-	for software in $install_things; do
-		deploy $software || confirm "Do you want to continue?" || exit 1
-	done
 }
 
 # vim: set filetype=sh ts=4 sw=4 noexpandtab:
