hust-open-atom-club/hustmirror-cli

commit 548868a00596e0c6a78b70e61039bde4fea37c3b

feat: support for Anolis OS
- Note that tests are not added since Anolis OS has mutiple config
 files, which has conflict with current testing framework.

Signed-off-by: jingfelix <jingfelix@outlook.com>

src/mirrors/anolis.sh
@@ -1,0 +1,86 @@
+_anolis_configs=()
+
+_anolis_get_configs() {
+    local folder="/etc/yum.repos.d"
+
+    for file in "$folder"/*.repo; do
+        if [ -f "$file" ]; then
+        _anolis_configs+=("$file")
+        fi
+    done
+}
+
+check() {
+	source_os_release
+	[ "$NAME" = "Anolis OS" ]
+}
+
+install() {
+	set_sudo
+
+    _anolis_configs=()
+    _anolis_get_configs
+
+    for config_file in "${_anolis_configs[@]}"; do
+        $sudo cp ${config_file} ${config_file}.bak || {
+            print_error "Failed to backup ${config_file}"
+            return 1
+        }
+
+        new_file=$(sed -E "s|https?://([^/]+)|${http}://${domain}|" $config_file)
+        {
+            cat << EOF | $sudo tee ${config_file} > /dev/null
+# ${gen_tag}
+${new_file}
+EOF
+        } || {
+            print_error "Failed to add mirror to ${config_file}"
+            return 1
+        }
+    done
+}
+
+is_deployed() {
+
+    _anolis_configs=()
+    _anolis_get_configs
+
+    for config_file in "${_anolis_configs[@]}"; do
+        if $sudo grep -q "${gen_tag}" "${config_file}"; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+can_recover() {
+    _anolis_configs=()
+    _anolis_get_configs
+
+    for config_file in "${_anolis_configs[@]}"; do
+        if ! test -f "${config_file}.bak"; then
+            return 1
+        fi
+    done
+
+    return 0
+}
+
+uninstall() {
+	set_sudo
+    
+    _anolis_configs=()
+    _anolis_get_configs
+
+    for config_file in "${_anolis_configs[@]}"; do
+        $sudo mv ${config_file}.bak ${config_file} || {
+            print_error "Failed to recover ${config_file}"
+            return 1
+        }
+    done
+
+    return 0
+}
+
+# vim: set filetype=sh ts=4 sw=4 noexpandtab:

commit 0e48d330ad30ac4b468191a273862d46dd3f8560

fix: fix anolis bash language feature.
Fixes: 548868a00596 ("feat: support for Anolis OS")

src/mirrors/anolis.sh
@@ -1,14 +1,4 @@
-_anolis_configs=()
-
-_anolis_get_configs() {
-    local folder="/etc/yum.repos.d"
-
-    for file in "$folder"/*.repo; do
-        if [ -f "$file" ]; then
-        _anolis_configs+=("$file")
-        fi
-    done
-}
+_anolis_config_dir="/etc/yum.repos.d"
 
 check() {
 	source_os_release
@@ -18,10 +8,8 @@ check() {
 install() {
 	set_sudo
 
-    _anolis_configs=()
-    _anolis_get_configs
-
-    for config_file in "${_anolis_configs[@]}"; do
+    for config_file in ${_anolis_config_dir}/*.repo; do
+		[ -f "$config_file" ] || continue
         $sudo cp ${config_file} ${config_file}.bak || {
             print_error "Failed to backup ${config_file}"
             return 1
@@ -42,10 +30,8 @@ EOF
 
 is_deployed() {
 
-    _anolis_configs=()
-    _anolis_get_configs
-
-    for config_file in "${_anolis_configs[@]}"; do
+    for config_file in ${_anolis_config_dir}/*.repo; do
+		[ -f "$config_file" ] || continue
         if $sudo grep -q "${gen_tag}" "${config_file}"; then
             return 0
         fi
@@ -55,10 +41,9 @@ is_deployed() {
 }
 
 can_recover() {
-    _anolis_configs=()
-    _anolis_get_configs
 
-    for config_file in "${_anolis_configs[@]}"; do
+    for config_file in ${_anolis_config_dir}/*.repo; do
+		[ -f "$config_file" ] || continue
         if ! test -f "${config_file}.bak"; then
             return 1
         fi
@@ -69,11 +54,9 @@ can_recover() {
 
 uninstall() {
 	set_sudo
-    
-    _anolis_configs=()
-    _anolis_get_configs
 
-    for config_file in "${_anolis_configs[@]}"; do
+    for config_file in ${_anolis_config_dir}/*.repo; do
+		[ -f "$config_file" ] || continue
         $sudo mv ${config_file}.bak ${config_file} || {
             print_error "Failed to recover ${config_file}"
             return 1